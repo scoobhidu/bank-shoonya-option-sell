7f46890767de93114d80b6714f141c0a78fde82c
rajat-kr <rajat.kumar@venturasecurities.com>
2025-07-21 08:16:46 +0530

    nifty selling

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,12 @@
 module.exports = {
+  watch: true,
+  watch_delay: 1000,
   apps: [
     {
       name: "fastapi-app",
       script: "uvicorn",
       args: "main:app --host 0.0.0.0 --port 8004", // Replace "main:app" with your app entry point
-
+      cron_restart: "0 9 * * 1-5",
       // Environment configuration
       interpreter: "python3", // Ensure you're using the right Python version
       exec_mode: "fork",      // 'fork' mode for running a single instance
